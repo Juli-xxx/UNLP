bfa5656216fde369f2dd464593eca7797d5230ac
Ju <julianamoroni.m@gmail.com>
2026-05-01 11:34:34 -0300

    Update and rename De cabeza con Java .java to DeCabezaConJava.java

--- Progra III/De cabeza con Java .java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("no le den tención a esto");
-    }
-}
---
+++ Progra III/DeCabezaConJava.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("no le den atención a esto xxxxxxxxxxx SIGO SIN ENCONTRAR EL PROBLEMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+}
