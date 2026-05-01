5756cee0e8e9db92714b06696b9e7becd2db5bd5
Ju <julianamoroni.m@gmail.com>
2026-05-01 11:33:09 -0300

    Update output message in VemosPascalDeNuevo.pas

--- Progra II /Vemos Pascal de nuevo.pas
+++
@@ -1,4 +0,0 @@
-program Hello;
-begin
-  WriteLn('Cuando se armen un github puede que entienda q es lo que hago con todo esto... .-~-.');
-end.
---
+++ Progra II /VemosPascalDeNuevo.pas
@@ -0,0 +1,4 @@
+program Hello;
+begin
+  WriteLn('Cuando se armen un github puede que entienda q es lo que hago con todo esto... .-~-. ROMPÍ ALGO Y NO SE QUEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
+end.
